f4e6ac5b75de414a396669ada703d35e4459b2a0
Ana2877 <acpagnoncelli@inf.ufrgs.br>
2023-03-13 20:44:14 -0300

    Separate router setup to another file

---
+++ src/router.setup.ts
@@ -0,0 +1,40 @@
+import express, { Express } from "express";
+import morgan from "morgan";
+import { userRouter } from "./users";
+
+export const router: Express = express();
+
+/** Logging */
+router.use(morgan("dev"));
+/** Parse the request */
+router.use(express.urlencoded({ extended: false }));
+/** Takes care of JSON data */
+router.use(express.json());
+
+/** API RULES */
+router.use((req, res, next) => {
+  // set the CORS policy
+  res.header("Access-Control-Allow-Origin", "*");
+  // set the CORS headers
+  res.header(
+    "Access-Control-Allow-Headers",
+    "origin, X-Requested-With,Content-Type,Accept, Authorization"
+  );
+  // set the CORS method headers
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "GET PATCH DELETE POST");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+/** Routes */
+router.use("/users", userRouter);
+
+/** Error handling */
+router.use((req, res) => {
+  const error = new Error("not found");
+  return res.status(404).json({
+    message: error.message
+  });
+});
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,8 @@
 /** source/server.ts */
 import dotenv from "dotenv";
 import http from "http";
-import express, { Express } from "express";
-import morgan from "morgan";
 import mongoose from "mongoose";
-import { userRouter } from "./users";
+import { router } from "./router.setup";
 import { uri } from "../database.config";
 
 // load the environment variables from the .env file
@@ -18,43 +16,6 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", function () {
   console.log("Connected successfully to database");
-});
-
-const router: Express = express();
-
-/** Logging */
-router.use(morgan("dev"));
-/** Parse the request */
-router.use(express.urlencoded({ extended: false }));
-/** Takes care of JSON data */
-router.use(express.json());
-
-/** RULES OF OUR API */
-router.use((req, res, next) => {
-  // set the CORS policy
-  res.header("Access-Control-Allow-Origin", "*");
-  // set the CORS headers
-  res.header(
-    "Access-Control-Allow-Headers",
-    "origin, X-Requested-With,Content-Type,Accept, Authorization"
-  );
-  // set the CORS method headers
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "GET PATCH DELETE POST");
-    return res.status(200).json({});
-  }
-  next();
-});
-
-/** Routes */
-router.use("/users", userRouter);
-
-/** Error handling */
-router.use((req, res) => {
-  const error = new Error("not found");
-  return res.status(404).json({
-    message: error.message
-  });
 });
 
 /** Server */
