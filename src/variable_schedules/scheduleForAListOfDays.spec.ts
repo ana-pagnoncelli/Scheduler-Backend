51a5c2e4f928206e0a37ea0779f0336fccc72476
Ana2877 <acpagnoncelli@inf.ufrgs.br>
2023-12-16 17:00:45 -0300

    start test for sorting

--- src/variable_schedules/scheduleForAListOfDays.spec.ts
+++ src/variable_schedules/scheduleForAListOfDays.spec.ts
@@ -7,8 +7,37 @@ import {
 } from "./fixtures";
 import { scheduleData } from "../fixed_schedules/fixtures";
 import { SchedulesReturn } from "./variableSchedule";
+import { ScheduleType } from "../fixed_schedules";
 
 describe("Variable Schedules", () => {
+  describe("test sortings ", () => {
+    it("should sort the schedules by hour", () => {
+      const scheduleList: Array<ScheduleType> = [
+        {
+          id: "1",
+          week_day: "MONDAY",
+          hour_of_the_day: "18:00",
+          users_list: [],
+          number_of_spots: 1
+        },
+        {
+          id: "2",
+          week_day: "MONDAY",
+          hour_of_the_day: "17:00",
+          users_list: [],
+          number_of_spots: 1
+        },
+        {
+          id: "3",
+          week_day: "MONDAY",
+          hour_of_the_day: "17:30",
+          users_list: [],
+          number_of_spots: 1
+        }
+      ];
+    });
+  });
+
   describe("GET variableSchedules/forAListOfDays ", () => {
     it("Should get the schedule for a list of days", async () => {
       const listOfDays = [
